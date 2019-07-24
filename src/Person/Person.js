3eb42a56e4ae35d1ab5258e51a78f8aff9def837
Fabian Wolter <fabiwolter@googlemail.com>
2019-07-24 16:48:39 +0200

    after merge

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Person.css'
 
-//ES6 way of writing the function as Arrow function:
-const person = (props) => {
-    return(
-        <div className="Person">
-            <p onClick={props.click}>I'm {props.name} and {props.age} years old!</p>
+import classes from './Person.css';
+
+const person = ( props ) => {
+    return (
+        <div className={classes.Person}>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
         </div>
